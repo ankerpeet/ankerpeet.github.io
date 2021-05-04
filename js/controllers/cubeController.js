598855885afe4e1b4c884898bac4a2291f319b01
Anker <ankerpeet@gmail.com>
2021-05-04 17:01:19 -0600

    added some routing stuff

--- js/controllers/cubeController.js
+++ js/controllers/cubeController.js
@@ -7,8 +7,18 @@ function CubeController() {
         education: false,
         experience: false,
         apps: false,
-        contact: false,
+        contact: false
     }
+
+    var descriptions = {
+        home: "Anker Peet currently works as a Software Engineer at Healthwise, a non-profit company located in Boise, Idaho.",
+        portfolio: "Chek out Anker Peet's portfolio which showcases Javascript, CSS, and HTML.",
+        education: "Anker Peet's education includes Brigham Young University - Idaho, Boise CodeWorks, and FreeCodeCamp.",
+        experience: "With over 4 years of profesional software and web development experience, Anker's work experience includes Healthwise, AgentCubed, and Peak Environmental.",
+        apps: "Outside of his day job, Anker Peet works on side projects which includes applications such as SurveySoft, and SimpleMoney.",
+        contact: "You can contact Anker Peet at me@ankerpeet.com"
+    }
+
     getPageParam();
     
     function rotate(tab) {
@@ -22,7 +32,57 @@ function CubeController() {
                 loadContent(tab);
             }
         }
+        updateMetaData(tab);
         updateClasses();
+        updateTabIndex();
+    }
+
+    function updateMetaData(tab) {
+        console.log(tab);
+        if(tabs.indexOf(tab) != -1) {
+            var pageName = uppercaseFirstLetter(tab);
+            document.title = "Anker Peet | Boise, Idaho | Software Developer | " + pageName + " Page";
+            console.log(descriptions[tab])
+            document.querySelector('meta[name="description"]').setAttribute('content', descriptions[tab]);
+        }
+    }
+
+    function uppercaseFirstLetter(tab) {
+        var arr = tab.split("");
+        arr[0] = arr[0].toUpperCase();
+        return arr.join("");
+    }
+
+    window.addEventListener('click', function (evt) {
+        if (!evt.target.classList.contains('spa-link')) return;
+      
+        evt.preventDefault();
+        var category = trimSlashes(evt.target.getAttribute('href'));
+        // if the category is empty, show the_keyword as the homepage.
+        if (category == '') category = 'home';
+        //showStoriesForCategory(category);
+        // update history
+        window.history.pushState({category: category}, window.title, evt.target.getAttribute('href'));
+        rotate(category);
+    });
+
+    window.addEventListener('popstate', function (evt) {
+        // if this history entry has 'state' (that is when you created it), use the state
+        // if it's a "real" browser history entry, find out what URL it comes from.
+        var category = evt.state ? evt.state.category : trimSlashes(window.location.pathname);
+        if (category == '') category = 'home';
+        rotate(category);
+    });
+
+    function trimSlashes(pathName) {
+        if (pathName[0] === '/') pathName = pathName.slice(1);
+        if (pathName.slice(-1) === '/') pathName = pathName.slice(-1);
+        return pathName;
+      }
+
+    function updateTabIndex() {
+        $(".tab :input:not(:hidden)").each(function (i) { $(this).attr('tabindex', -1); });
+        $(`.tab-${currentTab} :input:not(:hidden)`).each(function (i) { $(this).attr('tabindex', i + 1); });
     }
 
     function updateClasses() {
@@ -50,10 +110,10 @@ function CubeController() {
     }
 
     function addNavigation(id) {
-        var nav = `<button onclick="app.controllers.cubeController.changeTab('home')" id="logo">A</button>`;
+        var nav = `<a href='/home' id="logo" class='spa-link'>A</a>`;
         for(var i = 0; i < tabs.length; i++) {
             var tab = tabs[i];
-            nav += `<button onclick="app.controllers.cubeController.changeTab('${tab}')" id="${tab}" class="btn nav-link ${tab} ${tab == id ? "button-active" : ""}">${tab}</button>`
+            nav += `<a href='/${tab}' id="${tab}" class="spa-link btn nav-link ${tab} ${tab == id ? "button-active" : ""}">${tab}</a>`
         }
         nav += `<div class="socialContainer"><div class="socialLink"><a target="_blank" href="https://www.linkedin.com/in/anker-peet/"><i class="fa fa-linkedin-square"></i></a></div>`
         nav += `<div class="socialLink"><a target="_blank" href="https://github.com/ankerpeet"><i class="fa fa-github-square"></i></a></div></div>`
@@ -90,26 +150,26 @@ function CubeController() {
 
     function getPageParam() {
         var tab = "home"
-        if ('URLSearchParams' in window) {
-            var urlParams = new URLSearchParams(window.location.search);
-            if(urlParams.has('page')) {
-                var myParam = urlParams.get('page');
-                if(tabs.includes(myParam)) {
-                    tab = myParam;
-                }
-            }
-        }
+        // if ('URLSearchParams' in window) {
+        //     var urlParams = new URLSearchParams(window.location.search);
+        //     if(urlParams.has('page')) {
+        //         var myParam = urlParams.get('page');
+        //         if(tabs.includes(myParam)) {
+        //             tab = myParam;
+        //         }
+        //     }
+        // }
         rotate(tab);
         preloadContent();
     }
 
     function setPageParam(newTab) {
-        if ('URLSearchParams' in window) {
-            var searchParams = new URLSearchParams(window.location.search)
-            searchParams.set("page", newTab);
-            var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
-            history.pushState(null, '', newRelativePathQuery);
-        }
+        // if ('URLSearchParams' in window) {
+        //     var searchParams = new URLSearchParams(window.location.search)
+        //     searchParams.set("page", newTab);
+        //     var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+        //     history.pushState(null, '', newRelativePathQuery);
+        // }
     }   
 
     function preloadContent() {
